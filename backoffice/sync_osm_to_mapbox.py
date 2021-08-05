e45a52ca80228c16ebf0d84d8c2f4bfd2410616b
Cristian Klein <cristian.klein@elastisys.com>
2021-08-05 22:44:39 +0200

    Add sync code

--- backoffice/sync_osm_to_mapbox.py
+++ backoffice/sync_osm_to_mapbox.py
@@ -7,7 +7,7 @@ import requests
 
 QUERY='way["skating_comfort"]'
 MAPBOX_USER='anticristi'
-MAPBOX_TILESET_ID='ckrxgep2805sv21oja37n658t-9k84g'
+MAPBOX_TILESET_ID='skating_comfort'
 MAPBOX_ACCESS_TOKEN=os.environ['MAPBOX_ACCESS_TOKEN']
 
 logging.basicConfig(level=logging.DEBUG)
@@ -16,21 +16,17 @@ logger = logging.getLogger('sync_osm_to_mapbox')
 
 logger.info('Loading skating_comfort from OSM')
 api = overpass.API()
-response = api.get(QUERY, responseformat="geojson")
+response = api.get(QUERY, responseformat="geojson", verbosity='geom')
 
 logger.info('Got %d features', len(response["features"]))
 
 geoJsonLd = '\n'.join([ str(feature) for feature in response['features'] ])
-for line in geoJsonLd.splitlines():
-    logger.debug('Feature: %s', line)
+logger.info('Writing debug.geojson.ld')
+open('debug.geojson.ld', 'w').write(geoJsonLd)
 
-logger.info('Replacing Mapbox tileset')
+logger.info('Replacing Mapbox tileset source')
 
-# $ curl -X PUT "https://api.mapbox.com/tilesets/v1/sources/anticristi/hello-world?access_token=YOUR MAPBOX ACCESS TOKEN
-# This endpoint requires a token with tilesets:write scope.
-#  " \
-#     -F file=@/Users/username/data/mts/countries.geojson.ld \
-#     --header "Content-Type: multipart/form-data"
+# We use the same tileset source ID as the tileset ID.
 url = f'https://api.mapbox.com/tilesets/v1/sources/{MAPBOX_USER}/{MAPBOX_TILESET_ID}'
 params = { 'access_token': MAPBOX_ACCESS_TOKEN }
 files = { 'file': geoJsonLd }
@@ -39,6 +35,16 @@ r = requests.put(
     url = url,
     params = params,
     files = files,
+)
+logger.info('Got response with status_code %s', r.status_code)
+logger.info('Got response with body %s', r.json())
+
+logger.info('Publishing new tileset')
+url = f'https://api.mapbox.com/tilesets/v1/{MAPBOX_USER}.{MAPBOX_TILESET_ID}/publish'
+params = { 'access_token': MAPBOX_ACCESS_TOKEN }
+r = requests.post(
+    url = url,
+    params = params,
 )
 logger.info('Got response with status_code %s', r.status_code)
 logger.info('Got response with body %s', r.json())
